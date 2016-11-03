5150c475fcf3d3f85542c7735aaef7ee9101704f
kill0rz <kill0rz@users.noreply.github.com>
2016-11-03 14:35:54 +0100

    + Spiele können erst eingetragen werden, wenn das Spiel begonnen hat/vorbei ist (ACP, Ergebnis)

--- wbb/acp/wm2018_admin.php
+++ wbb/acp/wm2018_admin.php
@@ -357,16 +357,22 @@ if ($action == "result_add") {
 	$result = $db->query("SELECT gameid FROM bb" . $n . "_wm2018_spiele WHERE team_1_id AND team_2_id AND game_goals_1 != '' AND game_goals_2 != '' AND gameid < " . intval($_REQUEST['gameid']) . " ORDER BY datetime ASC");
 
 	$entry_games_until = intval($_REQUEST['gameid']) - 1;
-	if (intval($_REQUEST['gameid']) > 25 && $db->num_rows($result) != $entry_games_until) {
+	if (intval($_REQUEST['gameid']) > $gameid_vorrundenendspiel && $db->num_rows($result) != $entry_games_until) {
 		// error
 		eval("\$tpl->output(\"" . $tpl->get('wm2018_result_add_error', 1) . "\");");
 	} else {
-		// proceed
-		$wm2018_options = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_options");
-		$result = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
-		$result_1 = $db->query_first("SELECT name FROM bb" . $n . "_wm2018_teams WHERE teamid = '" . $result['team_1_id'] . "'");
-		$result_2 = $db->query_first("SELECT name FROM bb" . $n . "_wm2018_teams WHERE teamid = '" . $result['team_2_id'] . "'");
-		eval("\$tpl->output(\"" . $tpl->get('wm2018_result_add', 1) . "\");");
+		// Check, ob das Spiel schon vorbei ist:
+		$result = $db->query_first("SELECT datetime FROM bb" . $n . "_wm2018_spiele WHERE gameid = " . intval($_REQUEST['gameid']) . " LIMIT 1;");
+		if ($result['datetime'] + 6300 < time()) {
+			// proceed
+			$wm2018_options = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_options");
+			$result = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
+			$result_1 = $db->query_first("SELECT name FROM bb" . $n . "_wm2018_teams WHERE teamid = '" . $result['team_1_id'] . "'");
+			$result_2 = $db->query_first("SELECT name FROM bb" . $n . "_wm2018_teams WHERE teamid = '" . $result['team_2_id'] . "'");
+			eval("\$tpl->output(\"" . $tpl->get('wm2018_result_add', 1) . "\");");
+		} else {
+			eval("\$tpl->output(\"" . $tpl->get('wm2018_result_add_error_gamestillrunning', 1) . "\");");
+		}
 	}
 }
 
