19393101d6f99bd91242e10bb8289581ced85210
kill0rz <kill0rz@users.noreply.github.com>
2017-04-26 15:24:10 +0200

    refactor $gameids

--- wbb2/acp/wm2018_gameids.php
+++ wbb2/acp/wm2018_gameids.php
@@ -2,28 +2,24 @@
 
 // Konfiguration für die WM 2018
 
-$gameids = array();
-
-$gameids['vorrundenspiel'] = 48;
-
-$gameids['achtelfinal1'] = 49;
-$gameids['achtelfinal2'] = 50;
-$gameids['achtelfinal3'] = 51;
-$gameids['achtelfinal4'] = 52;
-$gameids['achtelfinal5'] = 53;
-$gameids['achtelfinal6'] = 54;
-$gameids['achtelfinal7'] = 55;
-$gameids['achtelfinal8'] = 56;
-
-$gameids['viertelfinal1'] = 57;
-$gameids['viertelfinal2'] = 58;
-$gameids['viertelfinal3'] = 59;
-$gameids['viertelfinal4'] = 60;
-
-$gameids['halbfinal1'] = 61;
-$gameids['halbfinal2'] = 62;
-
-$gameids['spielumplatzdrei'] = 63;
-$gameids['finale'] = 64;
+$gameids = array(
+	'vorrundenspiel' => 48,
+	'achtelfinal1' => 49,
+	'achtelfinal2' => 50,
+	'achtelfinal3' => 51,
+	'achtelfinal4' => 52,
+	'achtelfinal5' => 53,
+	'achtelfinal6' => 54,
+	'achtelfinal7' => 55,
+	'achtelfinal8' => 56,
+	'viertelfinal1' => 57,
+	'viertelfinal2' => 58,
+	'viertelfinal3' => 59,
+	'viertelfinal4' => 60,
+	'halbfinal1' => 61,
+	'halbfinal2' => 62,
+	'spielumplatzdrei' => 63,
+	'finale' => 64,
+);
 
 // Konfiguration Ende
