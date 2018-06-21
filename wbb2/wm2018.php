a012e09e40da40ae0393890f2fc78dfbdd6c10cd
kill0rz <kill0rz@kill0rz.com>
2018-06-21 19:20:22 +0200

    tendenzbildchen in gamedetails

--- wbb2/wm2018.php
+++ wbb2/wm2018.php
@@ -1521,8 +1521,8 @@ if ($action == "gamedetails") {
 	//1:1-Kopie von tippsprogame
 
 	// Tipps nur anzeigen, wenn Spiel schon gespielt
-	$result_datetime = $db->query_first("SELECT datetime FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
-	if ($result_datetime['datetime'] > $akttime) {
+	$result_gamedetails = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
+	if ($result_gamedetails['datetime'] > $akttime) {
 		redirect($lang->get("LANG_WM2018_PHP_60"), $url = "wm2018.php?action=showresults" . $SID_ARG_1ST);
 	}
 
@@ -1559,6 +1559,119 @@ if ($action == "gamedetails") {
 			}
 
 		}
+
+		// Tendenzbildchen
+		// Result
+
+		$ende = 0;
+		// +++++++++++++++++++ 1. Prüfung
+		// Tipp exakt richtig ?
+		if ($result_gamedetails['game_goals_1'] == $row['goals_1'] && $result_gamedetails['game_goals_2'] == $row['goals_2']) {
+			$tippright_result = "&nbsp;<img src=\"images/wm2018/thumbs_up.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_31']}\" title=\"{$lang->items['LANG_WM2018_PHP_31']}\" />";
+			$ende = 1;
+		}
+		// +++++++++++++++++++
+		// +++++++++++++++++++ 2. Prüfung
+		// Spiel unentschieden, Tipp unentschieden, Tendenz richtig ?
+		if ($ende == 0) {
+			if ($wm2018_options['tendenz'] == 1) {
+				if (($result_gamedetails['game_goals_1'] == $result_gamedetails['game_goals_2']) && ($row['goals_1'] == $row['goals_2'])) {
+					$tippright_result = "&nbsp;<img src=\"images/wm2018/tendenz.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_33']}\" title=\"{$lang->items['LANG_WM2018_PHP_33']}\" />";
+					$ende = 1;
+				}
+			}
+			if ($wm2018_options['tendenz'] == 0) {
+				if (($result_gamedetails['game_goals_1'] == $result_gamedetails['game_goals_2']) && ($row['goals_1'] == $row['goals_2'])) {
+					$tippright_result = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+					$ende = 1;
+				}
+			}
+		}
+		// +++++++++++++++++++
+		// +++++++++++++++++++ 3. Prüfung
+		// Spiel unentschieden, Tipp Sieg
+		if ($ende == 0) {
+			if (($result_gamedetails['game_goals_1'] == $result_gamedetails['game_goals_2']) && ($row['goals_1'] != $row['goals_2'])) {
+				$tippright_result = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+				$ende = 1;
+			}
+		}
+		// +++++++++++++++++++
+		// +++++++++++++++++++ 4. Prüfung
+		// Spiel Sieg, Tipp Sieg (falsch), Tendenz richtig ?
+		if ($ende == 0) {
+			if ($wm2018_options['tendenz'] == 1) {
+				if (($result_gamedetails['game_goals_1'] < $result_gamedetails['game_goals_2']) && ($row['goals_1'] < $row['goals_2']) || ($result_gamedetails['game_goals_1'] > $result_gamedetails['game_goals_2']) && ($row['goals_1'] > $row['goals_2'])) {
+					$tippright_result = "&nbsp;<img src=\"images/wm2018/tendenz.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_33']}\" title=\"{$lang->items['LANG_WM2018_PHP_33']}\" />";
+					$ende = 1;
+				}
+			}
+			if ($wm2018_options['tendenz'] == 0) {
+				if (($result_gamedetails['game_goals_1'] < $result_gamedetails['game_goals_2']) && ($row['goals_1'] < $row['goals_2']) || ($result_gamedetails['game_goals_1'] > $result_gamedetails['game_goals_2']) && ($row['goals_1'] > $row['goals_2'])) {
+					$tippright_result = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+					$ende = 1;
+				}
+			}
+		}
+		// +++++++++++++++++++
+		// +++++++++++++++++++ 5. Prüfung
+		// Spiel Sieg, Tipp Niederlage
+		// Siel Niederlage, Tipp Sieg
+		// Spiel Sieg, Tipp unentschieden
+		if ($ende == 0) {
+			if (($result_gamedetails['game_goals_1'] < $result_gamedetails['game_goals_2']) && ($row['goals_1'] > $row['goals_2']) || ($result_gamedetails['game_goals_1'] > $result_gamedetails['game_goals_2']) && ($row['goals_1'] < $row['goals_2']) || ($result_gamedetails['game_goals_1'] != $result_gamedetails['game_goals_2']) && ($row['goals_1'] == $row['goals_2'])) {
+				$tippright_result = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+				$ende = 1;
+			}
+		}
+
+		// +++++++++++++++++++
+
+		if ($wm2018_options['gk_jn'] == 1) {
+			if ($row['gk'] == 0) {
+				$game_gk = "<img src=\"images/wm2018/notok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_30']}\" title=\"{$lang->items['LANG_WM2018_PHP_30']}\" />";
+
+			}
+			if ($row['gk'] == 1) {
+				$game_gk = "<img src=\"images/wm2018/ok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_29']}\" title=\"{$lang->items['LANG_WM2018_PHP_29']}\" />";
+			}
+			// Prüfung, ob User richtig lag
+			if ($row['gk'] == $result_gamedetails['game_gk']) {
+				$tippright_gk = "&nbsp;<img src=\"images/wm2018/thumbs_up.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_31']}\" title=\"{$lang->items['LANG_WM2018_PHP_31']}\" />";
+			} else {
+				$tippright_gk = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+			}
+
+		}
+		if ($wm2018_options['rk_jn'] == 1) {
+			if ($row['rk'] == 0) {
+				$game_rk = "<img src=\"images/wm2018/notok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_30']}\" title=\"{$lang->items['LANG_WM2018_PHP_30']}\" />";
+			}
+			if ($row['rk'] == 1) {
+				$game_rk = "<img src=\"images/wm2018/ok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_29']}\" title=\"{$lang->items['LANG_WM2018_PHP_29']}\" />";
+			}
+			// Prüfung, ob User richtig lag
+			if ($row['rk'] == $result_gamedetails['game_rk']) {
+				$tippright_rk = "&nbsp;<img src=\"images/wm2018/thumbs_up.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_31']}\" title=\"{$lang->items['LANG_WM2018_PHP_31']}\" />";
+			} else {
+				$tippright_rk = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+			}
+		}
+		if ($wm2018_options['elfer_jn'] == 1) {
+			if ($row['elfer'] == 0) {
+				$game_elfer = "<img src=\"images/wm2018/notok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_30']}\" title=\"{$lang->items['LANG_WM2018_PHP_30']}\" />";
+			}
+			if ($row['elfer'] == 1) {
+				$game_elfer = "<img src=\"images/wm2018/ok.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_29']}\" title=\"{$lang->items['LANG_WM2018_PHP_29']}\" />";
+			}
+			// Prüfung, ob User richtig lag
+			if ($row['elfer'] == $result_gamedetails['game_elfer']) {
+				$tippright_elfer = "&nbsp;<img src=\"images/wm2018/thumbs_up.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_31']}\" title=\"{$lang->items['LANG_WM2018_PHP_31']}\" />";
+			} else {
+				$tippright_elfer = "&nbsp;<img src=\"images/wm2018/thumbs_down.gif\" border=\"0\" alt=\"{$lang->items['LANG_WM2018_PHP_32']}\" title=\"{$lang->items['LANG_WM2018_PHP_32']}\" />";
+			}
+		}
+
 		if (!empty($result_gamedetails['game_goals_1'])) {
 			eval("\$wm2018_tippsprogame_bit .= \"" . $tpl->get("wm2018_tippsprogame_bit") . "\";");
 		} else {
@@ -1573,12 +1686,11 @@ if ($action == "gamedetails") {
 // ++++++++++++++++++++++++++++++++++++++++++++
 if ($action == "tippsprogame") {
 	// Tipps nur anzeigen, wenn Spiel schon gespielt
-	$result_datetime = $db->query_first("SELECT datetime FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
-	if ($result_datetime['datetime'] > $akttime) {
+	$result_gamedetails = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
+	if ($result_gamedetails['datetime'] > $akttime) {
 		redirect($lang->get("LANG_WM2018_PHP_60"), $url = "wm2018.php?action=showresults" . $SID_ARG_1ST);
 	}
 
-	$result_gamedetails = $db->query_first("SELECT * FROM bb" . $n . "_wm2018_spiele WHERE gameid = '" . intval($_REQUEST['gameid']) . "'");
 	$result = $db->query("SELECT ut.*,u.username FROM bb" . $n . "_wm2018_usertipps ut LEFT JOIN bb" . $n . "_users u ON ut.userid=u.userid WHERE gameid = '" . intval($_REQUEST['gameid']) . "' ORDER BY userid ASC");
 	while ($row = $db->fetch_array($result)) {
 		$rowclass = getone($count++, "tablea", "tableb");
